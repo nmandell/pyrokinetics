e081ff994a9ce1c3311cffc47b7bc5403466f6e6
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-06-24 17:17:42 +0100

    Update real space FFT

--- pyrokinetics/examples/example_real_space_moment.py
+++ pyrokinetics/examples/example_real_space_moment.py
@@ -7,7 +7,10 @@ import xarray as xr
 # Load in file
 pyro = Pyro(gk_file="input.cgyro")
 
-
+ky = pyro.numerics.ky
+shat = pyro.local_geometry.shat
+Delta =  1 / (ky * shat)
+length = pyro.cgyro_input['BOX_SIZE'] * Delta
 a_lte = pyro.local_species.electron.a_lt
 
 
@@ -20,7 +23,15 @@ data = pyro.gk_output.data
 energy = data['moments'].sel(species='electron', drop=True).sel(moment='energy')
 energy = energy.where(energy.time > 500, drop=True).mean(dim=['time', 'theta']).sel(kx=energy.kx[1:])
 
-rs_energy = np.real(xrft.fft(energy))
+
+# Does inverse FFT (c2c along kx, c2r along ky)
+rs_energy = xrft.ifft(energy, real_dim='ky')
+
+nx = len(rs_energy.freq_kx.data)
+x = np.linspace(-1/2, 1/2, nx) * length
+
+rs_energy = rs_energy.assign_coords(freq_kx=x)
+
 rs_energy.plot(x='freq_kx')
 plt.xlabel('$x$')
 plt.ylabel(r'$y$')
@@ -28,37 +39,21 @@ plt.title(r'$\delta E_e$')
 plt.grid()
 plt.show()
 
+rs_energy.mean(dim='freq_ky').plot(x='freq_kx')
+plt.xlabel('$x$')
+plt.title(r'$\delta E_e$')
+plt.grid()
+plt.show()
+
 
 gradT = energy * energy.kx * 1j
-rs_gradT = a_lte - np.real(xrft.fft(gradT)).mean(dim='freq_ky')
+rs_gradT = a_lte - xrft.ifft(gradT, real_dim='ky').mean(dim='freq_ky')
+
+rs_gradT = rs_gradT.assign_coords(freq_kx=x)
 rs_gradT.plot()
+plt.vlines([-Delta, 0.0, Delta], ymin=min(rs_gradT.data), ymax=max(rs_gradT.data))
 plt.xlabel('$x$')
 plt.ylabel(r'$\omega_{Te}^{eff}$')
 plt.grid()
-plt.show()
-
-
-# Include -ky in FFT
-# Flip in kx, set ky -> -ky and take conjugate
-energy_flip = energy.copy()
-energy_flip = energy_flip.assign_coords(ky=-energy_flip.ky).assign_coords(kx=-energy_flip.kx)
-energy_flip = energy_flip.reindex(ky=energy_flip.ky[::-1], kx=energy_flip.kx[::-1]).where(energy_flip.ky != -0.0, drop=True)
-energy_full = xr.concat([np.conj(energy_flip), energy], dim='ky')
-
-
-rs_energy = np.real(xrft.fft(energy_full)) * 0.5
-rs_energy.plot(x='freq_kx')
-plt.show()
-
-
-gradT = energy_full * energy_full.kx * 1j
-rs_gradT = a_lte - np.real(xrft.fft(gradT).mean(dim='freq_ky'))
-rs_gradT.plot()
-plt.xlabel('$x$')
-plt.ylabel(r'$\omega_{Te}^{eff}$')
-plt.grid()
-plt.show()
-
-np.real(energy_full).plot()
 plt.show()
 
