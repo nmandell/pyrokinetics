7338a0f95e0e278433e55b7b99cd3140fad6c925
bpatel2107 <bpatel2107@users.noreply.github.com>
2022-06-24 16:19:42 +0000

    [skip ci] Apply black changes

--- pyrokinetics/examples/example_real_space_moment.py
+++ pyrokinetics/examples/example_real_space_moment.py
@@ -9,8 +9,8 @@ pyro = Pyro(gk_file="input.cgyro")
 
 ky = pyro.numerics.ky
 shat = pyro.local_geometry.shat
-Delta =  1 / (ky * shat)
-length = pyro.cgyro_input['BOX_SIZE'] * Delta
+Delta = 1 / (ky * shat)
+length = pyro.cgyro_input["BOX_SIZE"] * Delta
 a_lte = pyro.local_species.electron.a_lt
 
 
@@ -28,31 +28,31 @@ energy = (
 )
 
 # Does inverse FFT (c2c along kx, c2r along ky)
-rs_energy = xrft.ifft(energy, real_dim='ky')
+rs_energy = xrft.ifft(energy, real_dim="ky")
 
 nx = len(rs_energy.freq_kx.data)
-x = np.linspace(-1/2, 1/2, nx) * length
+x = np.linspace(-1 / 2, 1 / 2, nx) * length
 
 rs_energy = rs_energy.assign_coords(freq_kx=x)
-rs_energy.plot(x='freq_kx')
-plt.xlabel('$x$')
-plt.ylabel(r'$y$')
-plt.title(r'$\delta E_e$')
+rs_energy.plot(x="freq_kx")
+plt.xlabel("$x$")
+plt.ylabel(r"$y$")
+plt.title(r"$\delta E_e$")
 plt.grid()
 plt.show()
 
-rs_energy.mean(dim='freq_ky').plot(x='freq_kx')
-plt.xlabel('$x$')
-plt.title(r'$\delta E_e$')
+rs_energy.mean(dim="freq_ky").plot(x="freq_kx")
+plt.xlabel("$x$")
+plt.title(r"$\delta E_e$")
 plt.grid()
 plt.show()
 
 gradT = energy * energy.kx * 1j
-rs_gradT = a_lte - xrft.ifft(gradT, real_dim='ky').mean(dim='freq_ky')
+rs_gradT = a_lte - xrft.ifft(gradT, real_dim="ky").mean(dim="freq_ky")
 rs_gradT = rs_gradT.assign_coords(freq_kx=x)
 rs_gradT.plot()
 plt.vlines([-Delta, 0.0, Delta], ymin=min(rs_gradT.data), ymax=max(rs_gradT.data))
-plt.xlabel('$x$')
-plt.ylabel(r'$\omega_{Te}^{eff}$')
+plt.xlabel("$x$")
+plt.ylabel(r"$\omega_{Te}^{eff}$")
 plt.grid()
 plt.show()
